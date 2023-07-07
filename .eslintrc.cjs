62686523abb8af5b5a3b7359d7566786e3659728
luochao <1055120207@qq.com>
2023-07-07 17:24:09 +0800

    feat: 修改eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,16 +5,22 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { 
     ecmaVersion: 'latest', 
-    sourceType: 'module'
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
   },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
     '@typescript-eslint/ban-ts-comment': 'off'
   },
 }
