2648e29ca59485f0c4afe3fc4743ab2a7f8a46ed
luochao <1055120207@qq.com>
2023-07-19 17:57:25 +0800

    test: 测试husky

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -19,6 +19,7 @@ export function Login() {
   return (
     <div>
       <h3 css={{ fontSize: 24 }}>this is login page</h3>
+      <div>测试husky</div>
       <div>
         <Link to="/main/module1">goto module1</Link>
       </div>
