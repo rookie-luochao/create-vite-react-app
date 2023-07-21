588575c008472ef38ae9a8f9fe3ab7ee2b6de581
luochao <1055120207@qq.com>
2023-07-21 11:21:18 +0800

    chore: 更新eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,13 @@
-/* eslint-env node */
-
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
   ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { 
-    ecmaVersion: 'latest', 
-    sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -22,5 +15,5 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/ban-ts-comment': 'off'
-  },
+  }
 }
