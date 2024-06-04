f98c34507732d536831322f0e9fb3bcbf1a848f9
luochao <1055120207@qq.com>
2024-06-04 23:21:04 +0800

    feat: add import sort plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@request": "../core/http/request.ts",
+      "@request": resolve(__dirname, "src/core/http/request.ts"),
     },
   },
 });
