f6781727b92d7a36a2e6de4749badcf4b2e5c945
luochao <1055120207@qq.com>
2024-08-03 21:54:17 +0800

    chore: update react-query config

--- src/core/http/TanStackQuery.tsx
+++ src/core/http/TanStackQuery.tsx
@@ -1,15 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export function TanStackQueryProvider({ children }: { children: React.ReactNode }) {
+export function TanStackQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        refetchOnWindowFocus: false,
-        staleTime: 60 * 60 * 1000,
-        retry: false,
+        staleTime: 0, // 数据立即过期，单位：毫秒
+        retry: 1, // 请求失败时重试一次
+        retryDelay: 1000, // 重试延迟时间为1秒
+        refetchOnWindowFocus: true, // 窗口重新获得焦点时重新获取数据
+        refetchOnReconnect: true, // 网络重新连接时是否重新获取数据
       },
     },
   });
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
